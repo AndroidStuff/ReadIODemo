bef20826f1b9ba95e1855e0e3a1e73f227904d99
Karthik Sirasanagandla <sirkarthik@gmail.com>
2016-04-15 18:46:23 +0530

    Implemented code to read text file and display contents in view

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 ## About
 
 Demo varied Read IO options (Text, JSON and XML files) with Android libraries
+
+## Steps to reading a Text file
+* Get a handle to `AssetManager` using `getAssets()`
+* Open file in `assets` folder using the `AssetManager.open(filename)` method
+* Rest of the stuff is the routine core java library API stuff; you read using `BufferedReader` and loop through the contents using `readLine()`
+
+## Steps to reading an XML file
+## Steps to reading a JSON file
+
+## Resources
+* The sample text in sample_text.txt is a generated one, got from the website [BlindTextGenerator.com](http://www.blindtextgenerator.com/lorem-ipsum). Try it to <3 it :)
--- src/com/example/readiodemo/MainActivity.java
+++ src/com/example/readiodemo/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.readiodemo;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	private TextView textView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		textView = (TextView) findViewById(R.id.textView);
+	}
+
+	private void clearText() {
+		textView.setText("");
+	}
+
+	// onClick Event Handler or UI Button element
+	public void readTextFile(View v) {
+		String text = readFileFromAssets("sample_text.txt");
+		clearText();
+		textView.setText(text);
+	}
+
+	public String readFileFromAssets(String fileName) {
+		BufferedReader reader = null;
+		StringBuilder sb = new StringBuilder("");
+		String line;
+		try {
+			final InputStream fileStream = getAssets().open(fileName);
+			reader = new BufferedReader(new InputStreamReader(fileStream));
+			while(true) {
+				line = reader.readLine();
+				if(line == null) break;
+				sb.append(line).append("\n");
+			}
+			return sb.toString();
+		} catch (IOException e) {
+		} finally {
+			try {
+				reader.close();
+			} catch (IOException e) {}
+		}
+		return "";
 	}
 }
